0f14fb02acf583bc544c79e2325524eca4413c13
ngoctu280801 <ngoctu.280801@gmail.com>
2025-05-21 21:21:29 +0700

    fix err Entire page deopted

--- app/components/reusables/PageLoader/PageLoader.tsx
+++ app/components/reusables/PageLoader/PageLoader.tsx
@@ -1,45 +1,10 @@
-"use client";
-
-import { Suspense, useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import { spinnerService } from "@/services/spinner.service";
+import { Suspense } from "react";
+import Spinner from "../Spinner/Spinner";
 
 export default function PageLoader({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    spinnerService.startSpinner();
-    const timer = setTimeout(() => {
-      spinnerService.endSpinner();
-    }, 500);
-
-    return () => clearTimeout(timer);
-  }, [pathname, searchParams]);
-
-  return (
-    <Suspense
-      fallback={
-        <div>
-          <LoadingSpinner />
-          {children}
-        </div>
-      }
-    >
-      {children}
-    </Suspense>
-  );
-}
-
-function LoadingSpinner() {
-  useEffect(() => {
-    spinnerService.startSpinner();
-    return () => spinnerService.endSpinner();
-  }, []);
-
-  return null;
+  return <Suspense fallback={<Spinner />}>{children}</Suspense>;
 }
