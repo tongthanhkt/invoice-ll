40c856b32d53d8be09462ac7da40476c2bcfe4b7
ngoctu280801 <63794942+ngoctu280801@users.noreply.github.com>
2025-05-23 00:03:15 +0700

    fix not found error SSR

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,10 @@
-'use client';
-
-import Error from 'next/error';
-
 export default function NotFound() {
-  return <Error statusCode={404} />;
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center h-screen -mt-10">
+      <h1 className="text-2xl font-bold">404 - Not Found</h1>
+      <p className="text-sm text-gray-500">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
 }
