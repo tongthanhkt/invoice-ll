5035a813d3978659d733fb8f21a1f069f5d7a468
ngoctu280801 <ngoctu.280801@gmail.com>
2025-05-31 19:23:08 +0700

    client info template api

---
+++ app/models/ClientInfoTemplate.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const clientInfoTemplateSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+
+  // contact name
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  user_id: { type: String },
+  address: { type: String },
+  phone_number: { type: String },
+  company_name: { type: String },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create and export the User Info Template model
+export default mongoose.models.ClientInfoTemplate ||
+  mongoose.model("ClientInfoTemplate", clientInfoTemplateSchema);
